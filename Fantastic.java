af6b08f9e9c25f0f232c6afe4eaa465a23cfc9c8
nithu33 <42604896+nithu33@users.noreply.github.com>
2018-08-23 21:02:57 +0530

    Add files via upload

---
+++ Fantastic.java
@@ -0,0 +1,54 @@
+package inhe;
+
+public class Fantastic {
+	
+String name="srinithi.B";
+String hobby="Dance";
+int age=18;
+String branch="CSE";
+double marks=92.3;
+public void setname(String n)      //using get set method to get the student details
+{
+	name=n;
+}
+public String getname()
+{
+	return name;
+}
+public void sethobby(String h)
+{
+	hobby=h;
+}
+public String gethobby()
+{
+	return hobby;
+}
+public void setage(int a)
+{
+	age=a;
+}
+public int getage()
+{
+	return age;
+}
+public void setbranch(String b)
+{
+	branch=b;
+}
+public String getbranch()
+{
+	return branch;
+}
+public void setmarks(double m)
+{
+	marks=m;
+}
+public double getmarks()
+{
+	return marks;
+}
+public void activities()
+{
+	System.out.println("studying");
+}
+}
